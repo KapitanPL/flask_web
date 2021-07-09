86e1eae596d0bc6ff837178018987fbca3b44a49
Stepan Svoboda <svobost@gmail.com>
2021-07-09 11:33:06 +0200

    working version - pre bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from app.config import Config
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
