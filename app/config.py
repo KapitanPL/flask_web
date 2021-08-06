908a2650c7ffc981be79bdfa97b5b280363c36b6
Stepan Svoboda <stepan.svoboda@memsource.com>
2021-08-06 21:46:59 +0200

    config

--- app/config.py
+++ app/config.py
@@ -2,12 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    TESTING=True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Should_I!really09Care?'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY') or 'public'
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY') or 'private'
-    RECAPTCHA_OPTIONS = {'theme': 'white'}
     UPLOAD_EXTENSIONS = ['.md']
