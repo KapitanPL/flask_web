12c5a0511417825adc76080553273db6346ced74
Stepan Svoboda <stepan.svoboda@memsource.com>
2021-08-27 22:05:13 +0200

    collation

--- app/models.py
+++ app/models.py
@@ -137,7 +137,7 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.String(128), index=True, unique=True)
     used = db.Column(db.Integer, default=1)
-    description = db.Column(db.Unicode(2048), index=False, unique=False,default="Add description...")
+    description = db.Column(db.Unicode(2048), collation='utf8_bin', index=False, unique=False,default="Add description...")
     articles = db.relationship("Article",
         secondary=tagged,
         back_populates="tags")
